b94f46cef1af09e5164f5d74668b3c7a8b438d4f
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-07-05 23:03:56 +0200

    migrate full ovos_utils.configuration module (#7)
    migrate full ovos_utils.configuration module

--- ovos_config/config.py
+++ ovos_config/config.py
@@ -13,219 +13,18 @@
 # limitations under the License.
 #
 import json
-import re
-import yaml
 from os.path import isfile
-from mycroft_bus_client.message import Message
-from ovos_config.locations import *
-from ovos_utils.configuration import get_xdg_config_locations, get_xdg_config_save_path
-from ovos_utils.network_utils import is_connected
-from ovos_utils import camel_case_split
-from ovos_utils.json_helper import load_commented_json, merge_dict
-from ovos_utils.log import LOG
-from ovos_utils.json_helper import flattened_delete
+from time import sleep
+
+from mycroft_bus_client import Message
+
+from ovos_config.base import LocalConf, MycroftDefaultConfig, MycroftSystemConfig, MycroftUserConfig, RemoteConf
+from ovos_config.locations import OLD_USER_CONFIG, get_xdg_config_save_path, get_xdg_config_locations
 from ovos_config.utils import FileWatcher
 
-try:
-    import mycroft.api as selene_api
-except ImportError:
-    # TODO https://github.com/OpenVoiceOS/selene_api
-    selene_api = None
-
-
-def is_remote_list(values):
-    """Check if list corresponds to a backend formatted collection of dicts
-    """
-    for v in values:
-        if not isinstance(v, dict):
-            return False
-        if "@type" not in v.keys():
-            return False
-    return True
-
-
-def translate_remote(config, setting):
-    """Translate config names from server to equivalents for mycroft-core.
-
-    Args:
-        config:     base config to populate
-        settings:   remote settings to be translated
-    """
-    IGNORED_SETTINGS = ["uuid", "@type", "active", "user", "device"]
-
-    for k, v in setting.items():
-        if k not in IGNORED_SETTINGS:
-            # Translate the CamelCase values stored remotely into the
-            # Python-style names used within mycroft-core.
-            key = re.sub(r"Setting(s)?", "", k)
-            key = camel_case_split(key).replace(" ", "_").lower()
-            if isinstance(v, dict):
-                config[key] = config.get(key, {})
-                translate_remote(config[key], v)
-            elif isinstance(v, list):
-                if is_remote_list(v):
-                    if key not in config:
-                        config[key] = {}
-                    translate_list(config[key], v)
-                else:
-                    config[key] = v
-            else:
-                config[key] = v
-
-
-def translate_list(config, values):
-    """Translate list formatted by mycroft server.
-
-    Args:
-        config (dict): target config
-        values (list): list from mycroft server config
-    """
-    for v in values:
-        module = v["@type"]
-        if v.get("active"):
-            config["module"] = module
-        config[module] = config.get(module, {})
-        translate_remote(config[module], v)
-
-
-class LocalConf(dict):
-    """Config dictionary from file."""
-
-    def __init__(self, path):
-        super().__init__(self)
-        self.path = path
-        if path:
-            self.load_local(path)
-
-    def _get_file_format(self, path=None):
-        """The config file format
-        supported file extensions:
-        - json (.json)
-        - commented json (.conf)
-        - yaml (.yaml/.yml)
-
-        returns "yaml" or "json"
-        """
-        path = path or self.path
-        if not path:
-            return "dict"
-        if path.endswith(".yml") or path.endswith(".yaml"):
-            return "yaml"
-        else:
-            return "json"
-
-    def load_local(self, path=None):
-        """Load local json file into self.
-
-        Args:
-            path (str): file to load
-        """
-        path = path or self.path
-        if not path:
-            LOG.error(f"in memory configuration, nothing to load")
-            return
-        if exists(path) and isfile(path):
-            try:
-                if self._get_file_format(path) == "yaml":
-                    with open(path) as f:
-                        config = yaml.safe_load(f)
-                    if isinstance(config, str):
-                        LOG.error(f"yaml config parsed as str. '{path}'")
-                        with open(path) as f:
-                            contents = f.read()
-                        LOG.debug(f"{path}:\n{contents}")
-                        return
-                else:
-                    config = load_commented_json(path)
-                for key in config:
-                    self.__setitem__(key, config[key])
-                LOG.debug(f"Configuration {path} loaded")
-            except json.JSONDecodeError as e:
-                LOG.error(f"Error parsing json configuration '{path}': {e}")
-                with open(path) as f:
-                    contents = f.read()
-                LOG.debug(f"{path}:\n{contents}")
-            except Exception as e:
-                LOG.exception(f"Error loading configuration '{path}'")
-        else:
-            LOG.debug(f"Configuration '{path}' not defined, skipping")
-
-    def reload(self):
-        self.load_local(self.path)
-
-    def store(self, path=None):
-        """Cache the received settings locally.
-
-        The cache will be used if the remote is unreachable to load settings
-        that are as close to the user's as possible.
-        """
-        path = path or self.path
-        if not path:
-            LOG.error(f"in memory configuration, no save location")
-            return
-        if self._get_file_format(path) == "yaml":
-            with open(path, 'w') as f:
-                yaml.dump(dict(self), f, allow_unicode=True,
-                          default_flow_style=False, sort_keys=False)
-        else:
-            with open(path, 'w') as f:
-                json.dump(self, f, indent=2)
-
-    def merge(self, conf):
-        merge_dict(self, conf)
-
-
-class RemoteConf(LocalConf):
-    """Config dictionary fetched from mycroft.ai."""
-
-    def __init__(self, cache=WEB_CONFIG_CACHE):
-        super(RemoteConf, self).__init__(cache)
-
-    def reload(self):
-        if selene_api is None:
-            self.load_local(self.path)
-            return
-        try:
-            if not selene_api.is_paired():
-                self.load_local(self.path)
-                return
-
-            if selene_api.is_backend_disabled():
-                # disable options that require backend
-                config = {
-                    "server": {
-                        "metrics": False,
-                        "sync_skill_settings": False
-                    },
-                    "skills": {"upload_skill_manifest": False},
-                    "opt_in": False
-                }
-                for key in config:
-                    self.__setitem__(key, config[key])
-            else:
-                api = selene_api.DeviceApi()
-                setting = api.get_settings()
-                location = None
-                try:
-                    location = api.get_location()
-                except Exception as e:
-                    LOG.error(f"Exception fetching remote location: {e}")
-                    if exists(self.path) and isfile(self.path):
-                        location = load_commented_json(self.path).get('location')
-
-                if location:
-                    setting["location"] = location
-                # Remove server specific entries
-                config = {}
-                translate_remote(config, setting)
-
-                for key in config:
-                    self.__setitem__(key, config[key])
-                self.store(self.path)
-
-        except Exception as e:
-            LOG.error(f"Exception fetching remote configuration: {e}")
-            self.load_local(self.path)
+from ovos_utils.json_helper import flattened_delete, merge_dict
+from ovos_utils.log import LOG
+from ovos_utils.network_utils import is_connected
 
 
 def _log_old_location_deprecation(old_user_config=OLD_USER_CONFIG):
@@ -242,8 +41,8 @@ class Configuration(dict):
     """Namespace for operations on the configuration singleton."""
     __patch = LocalConf(None)  # Patch config that skills can update to override config
     bus = None
-    default = LocalConf(DEFAULT_CONFIG)
-    system = LocalConf(SYSTEM_CONFIG)
+    default = MycroftDefaultConfig()
+    system = MycroftSystemConfig()
     remote = RemoteConf()
     # This includes both the user config and
     # /etc/xdg/mycroft/mycroft.conf
@@ -492,7 +291,7 @@ class Configuration(dict):
                     Configuration.reload()
                 except:
                     LOG.exception("Failed to load configuration, syntax seems invalid!")
-        
+
         for handler in Configuration._callbacks:
             try:
                 handler()
@@ -564,3 +363,19 @@ class Configuration(dict):
         """DEPRECATED - there is no cache anymore """
         Configuration.updated(message)
 
+
+def read_mycroft_config():
+    """ returns a stateless dict with the loaded configuration """
+    return dict(Configuration())
+
+
+def update_mycroft_config(config, path=None):
+    """ updates user config file with the contents of provided dict
+    if a path is provided that location will be used instead of MycroftUserConfig"""
+    if path is None:
+        conf = MycroftUserConfig()
+    else:
+        conf = LocalConf(path)
+    conf.merge(config)
+    conf.store()
+    return conf
