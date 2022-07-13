07e2b6643f9e359e13e598aa542dd010c322ae92
Daniel McKnight <daniel@neon.ai>
2022-07-13 15:12:15 -0700

    Resolve test failures

--- test/unittests/test_locale.py
+++ test/unittests/test_locale.py
@@ -12,17 +12,21 @@ class TestLocale(TestCase):
     def test_get_get_primary_lang_code(self):
         import ovos_config.locale
         ovos_config.locale.LF = None
+        ovos_config.locale._lang = None
         self.assertEqual(
             ovos_config.locale.get_primary_lang_code(valid_config), 'es')
+        ovos_config.locale._lang = None
         self.assertEqual(
             ovos_config.locale.get_primary_lang_code(invalid_config), 'en')
 
     def test_get_default_lang(self):
         import ovos_config.locale
         ovos_config.locale.LF = None
+        ovos_config.locale._lang = None
         self.assertEqual(
-            ovos_config.locale.get_primary_lang_code(valid_config),
+            ovos_config.locale.get_default_lang(valid_config),
             valid_config['lang'])
+        ovos_config.locale._lang = None
         self.assertEqual(
-            ovos_config.locale.get_primary_lang_code(invalid_config), 'en-us')
+            ovos_config.locale.get_default_lang(invalid_config), 'en-us')
 
