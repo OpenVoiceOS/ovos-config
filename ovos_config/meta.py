8b896f9743b28adf7ec7f183e664b091eae54fb6
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-07-31 19:01:52 +0100

    update docstr

--- ovos_config/meta.py
+++ ovos_config/meta.py
@@ -15,32 +15,23 @@ Examples config:
    //         eg, OVOS, Neon, Chatterbox...
    //  all XDG paths should respect this
    //        {xdg_path}/{base_folder}/some_resource
-   // "mycroft.conf" paths are derived from this
-   //        ~/.{base_folder}/mycroft.conf
    "base_folder": "OpenVoiceOS",
 
    // the filename of "mycroft.conf",
    //      eg, ovos.conf, chatterbox.conf, neon.conf...
    // "mycroft.conf" paths are derived from this
-   //        ~/.{base_folder}/{config_filename}
+   //        {xdg_path}/{base_folder}/{config_filename}
    "config_filename": "mycroft.conf",
 
    // override the default.conf location, allows changing the default values
    //     eg, disable backend, disable skills, configure permissions
    "default_config_path": "/etc/OpenVoiceOS/default_mycroft.conf",
 
-   // this is intended for derivative products, if a module name is present
-   // in sys.modules then the values below will be used instead
-   //     eg, chatterbox/mycroft/ovos/neon can coexist in the same machine
+   // this is intended for derivative products, if ovos is being imported 
+   // from one of these modules then the values below will be used instead
+   //     eg, mycroft/ovos/neon can coexist in the same machine
    "module_overrides": {
-        "chatterbox": {
-            "xdg": false,
-            "base_folder": "chatterbox",
-            "config_filename": "chatterbox.conf",
-            "default_config_path": "/opt/chatterbox/chatterbox.conf"
-        },
         "neon_core": {
-            "xdg": true,
             "base_folder": "neon",
             "config_filename": "neon.conf",
             "default_config_path": "/opt/neon/neon.conf"
@@ -48,11 +39,6 @@ Examples config:
    },
    // essentially aliases for the above, useful for microservice architectures
    "submodule_mappings": {
-        "chatterbox_stt": "chatterbox",
-        "chatterbox_playback": "chatterbox",
-        "chatterbox_admin": "chatterbox",
-        "chatterbox_blockly": "chatterbox",
-        "chatterbox_gpio_service": "chatterbox",
         "neon_speech": "neon_core",
         "neon_audio": "neon_core",
         "neon_enclosure": "neon_core"
