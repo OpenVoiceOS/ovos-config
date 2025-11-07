5e572b073020864d1fc6a1cc7783f69d504e9c6a
miro <jarbasai@mailfence.com>
2025-11-07 02:11:39 +0000

    fix: mark deprecated code for removal

--- ovos_config/locale.py
+++ ovos_config/locale.py
@@ -1,22 +1,59 @@
+import warnings
+
 from dateutil.tz import gettz, tzlocal
+from ovos_utils.log import deprecated
 
 import ovos_config
 
-# lingua_franca is optional and might not be installed
-# exceptions should only be raised in the parse and format utils
-
-
-try:
-    import lingua_franca as LF
-except ImportError:
-    LF = None
-
-_lang = None
 _default_tz = None
 
 
+def get_config_tz():
+    code = ovos_config.Configuration()["location"]["timezone"]["code"]
+    return gettz(code)
+
+
+def get_default_tz():
+    # if default was set at runtime use it else use the timezone from .conf
+    return _default_tz or get_config_tz()
+
+
+def set_default_tz(tz=None):
+    """ configure timezone across OVOS packages
+
+    currently only configures lingua-franca, in the future
+    other hooks may be added if we need to perform this operation globally """
+    global _default_tz
+    tz = tz or get_config_tz() or tzlocal()
+    _default_tz = tz
+
+
+def get_valid_languages():
+    """ return all valid runtime languages according to mycroft.conf """
+    lang_code = ovos_config.Configuration().get("lang", "en-us")
+    extra_lang_codes = ovos_config.Configuration().get("secondary_langs", [])
+    return set([lang_code] + extra_lang_codes)
+
+
+@deprecated("deprecated, use ovos_config.Configuration() object directly", "1.0.0")
+def get_default_lang(config=None):
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated, use ovos_config.Configuration() object directly",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return ovos_config.Configuration().get("lang", "en-us")
+
+
+@deprecated("deprecated without replacement", "1.0.0")
 def get_full_lang_code(lang):
     """ given a 2-letter lang code, return the full default 4-letter code"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     # first give preference to any configured dialects
     # eg, pt-br instead of pt-pt
     valid_langs = get_valid_languages()
@@ -48,106 +85,57 @@ def get_full_lang_code(lang):
     return langmap.get(lang)
 
 
+@deprecated("deprecated without replacement", "1.0.0")
 def get_primary_lang_code(config=None):
-    global _lang
-    if not _lang:
-        config = config or ovos_config.Configuration()
-        _lang = config.get("lang", "en-us")
-    return _lang.split("-")[0]
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return get_default_lang()
 
 
-def get_default_lang(config=None):
-    """
-    Get the default localized language code (i.e. `en-us`)
-    @param config: Configuration (default is Configuration())
-    @return: lowercase BCP-47 language code
-    """
-    global _lang
-    if not _lang:
-        config = config or ovos_config.Configuration()
-        _lang = config.get("lang", "en-us")
-    return _lang
-
-
+@deprecated("deprecated without replacement", "1.0.0")
 def set_default_lang(lang):
-    """ setup default language across OVOS packages
-    
-    currently only configures lingua-franca language, in the future 
-    other hooks may be added if we need to perform this operation globally"""
-    global _lang
-    _lang = lang
-    if LF:
-        try:
-            LF.set_default_lang(lang)
-        except:
-            pass
-
-def get_config_tz():
-    code = ovos_config.Configuration()["location"]["timezone"]["code"]
-    return gettz(code)
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    ovos_config.Configuration()["lang"] = lang
 
 
-def get_default_tz():
-    # if default was set at runtime use it else use the timezone from .conf
-    return _default_tz or get_config_tz()
-
-
-def set_default_tz(tz=None):
-    """ configure timezone across OVOS packages
-    
-    currently only configures lingua-franca, in the future 
-    other hooks may be added if we need to perform this operation globally """
-    global _default_tz
-    tz = tz or get_config_tz() or tzlocal()
-    _default_tz = tz
-    if LF:
-        # tz added in recently, depends on version
-        try:
-            LF.time.set_default_tz(tz)
-        except:
-            pass
-
-
+@deprecated("deprecated without replacement", "1.0.0")
 def load_languages(langs):
-    """ load and configure lang specific resources across OVOS packages
-    
-    currently only loads lingua-franca language data, in the future 
-    other hooks may be added if we need to perform this operation globally"""
-    if LF:
-        try:
-            LF.load_languages(langs)
-        except:
-            pass
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
 
 
+@deprecated("deprecated without replacement", "1.0.0")
 def load_language(lang):
-    """ load and configure lang specific resources across OVOS packages
-    
-    currently only loads lingua-franca language data, in the future 
-    other hooks may be added if we need to perform this operation globally"""
-    if LF:
-        try:
-            LF.load_language(lang)
-        except:
-            pass
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
 
 
-def get_valid_languages():
-    """ return all valid runtime languages according to mycroft.conf """
-    lang_code = ovos_config.Configuration().get("lang", "en-us")
-    extra_lang_codes = ovos_config.Configuration().get("secondary_langs", [])
-    return set([lang_code] + extra_lang_codes)
-
-
+@deprecated("deprecated without replacement", "1.0.0")
 def setup_locale(lang=None, tz=None):
-    """ setup default language, timezone and other locale data across OVOS packages
-    
-    currently only configures lingua-franca, in the future 
-    other hooks may be added if we need to perform this operation globally"""
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     lang_code = lang or ovos_config.Configuration().get("lang", "en-us")
-    valid_langs = get_valid_languages()
-    # load any lang specific resources
-    load_languages(valid_langs)
     # Set the active lang to match the configured one
     set_default_lang(lang_code)
     # Set the default timezone to match the configured one
@@ -155,14 +143,11 @@ def setup_locale(lang=None, tz=None):
 
 
 # mycroft-core backwards compat LF only interface
+@deprecated("deprecated without replacement", "1.0.0")
 def set_default_lf_lang(lang_code="en-us"):
-    """Set the default language of Lingua Franca for parsing and formatting.
-
-    Note: this is a temporary method until a global set_default_lang() method
-    can be implemented that updates all Mycroft systems eg STT and TTS.
-    It will be deprecated at the earliest possible point.
-
-    Args:
-        lang (str): BCP-47 language code, e.g. "en-us" or "es-mx"
-    """
-    return set_default_lang(lang_code)
+    """DEPRECATED"""
+    warnings.warn(
+        "deprecated without replacement",
+        DeprecationWarning,
+        stacklevel=2,
+    )
